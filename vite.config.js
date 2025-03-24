dafd4f3c31a219d36bbc568eca25bba387b8bd08
mdishroon <hl2804@wayne.edu>
2025-03-23 21:29:33 -0400

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,6 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: path.resolve(dirname, "index.html"),
-				newRecipe: path.resolve(dirname, "recipes/new/index.html"),
 			},
 		},
 	},
