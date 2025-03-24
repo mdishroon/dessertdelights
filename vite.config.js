42a97296d4c4fdd16cd55446872342891474ebb9
mdishroon <hl2804@wayne.edu>
2025-03-23 21:52:26 -0400

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import path from "node:path";
 import url from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, ModuleNode } from "vite";
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
@@ -10,6 +10,15 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: path.resolve(dirname, "index.html"),
+				addRecipe: path.resolve(dirname, "addRecipe.html"),
+				cakeflavor: path.resolve(dirname, "cakeflavor.html"),
+				chocolatedessert: path.resolve(dirname, "chocolatedessert.html"),
+				contact: path.resolve(dirname, "contact.html"),	
+				drinkdes: path.resolve(dirname, "drinkdessert.html"),
+				moodboard: path.resolve(dirname, "moodboard.html"),
+				quizzes: path.resolve(dirname, "quizzeshomepage.html"),
+				recipe: path.resolve(dirname, "recipecards.html"),
+				thanks: path.resolve(dirname, "thankyou.html"),
 			},
 		},
 	},
